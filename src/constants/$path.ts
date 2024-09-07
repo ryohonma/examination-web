7810f06e2afd9367adbe88c253b72a2a72dbc947
ryohonma <ryohonma0830@gmail.com>
2024-09-07 23:50:58 +0900

    lint fix

--- src/constants/$path.ts
+++ src/constants/$path.ts
@@ -1,33 +1,56 @@
-const buildSuffix = (url?: {query?: Record<string, string>, hash?: string}) => {
+const buildSuffix = (url?: {
+  query?: Record<string, string>;
+  hash?: string;
+}) => {
   const query = url?.query;
   const hash = url?.hash;
-  if (!query && !hash) return '';
-  const search = query ? `?${new URLSearchParams(query)}` : '';
-  return `${search}${hash ? `#${hash}` : ''}`;
+  if (!query && !hash) return "";
+  const search = query ? `?${new URLSearchParams(query)}` : "";
+  return `${search}${hash ? `#${hash}` : ""}`;
 };
 
 export const pagesPath = {
-  "login": {
-    $url: (url?: { hash?: string }) => ({ pathname: '/login' as const, hash: url?.hash, path: `/login${buildSuffix(url)}` })
+  login: {
+    $url: (url?: { hash?: string }) => ({
+      pathname: "/login" as const,
+      hash: url?.hash,
+      path: `/login${buildSuffix(url)}`,
+    }),
   },
-  "profile": {
-    $url: (url?: { hash?: string }) => ({ pathname: '/profile' as const, hash: url?.hash, path: `/profile${buildSuffix(url)}` })
+  profile: {
+    $url: (url?: { hash?: string }) => ({
+      pathname: "/profile" as const,
+      hash: url?.hash,
+      path: `/profile${buildSuffix(url)}`,
+    }),
   },
-  "signup": {
-    $url: (url?: { hash?: string }) => ({ pathname: '/signup' as const, hash: url?.hash, path: `/signup${buildSuffix(url)}` })
+  signup: {
+    $url: (url?: { hash?: string }) => ({
+      pathname: "/signup" as const,
+      hash: url?.hash,
+      path: `/signup${buildSuffix(url)}`,
+    }),
   },
-  "timelines": {
-    $url: (url?: { hash?: string }) => ({ pathname: '/timelines' as const, hash: url?.hash, path: `/timelines${buildSuffix(url)}` })
+  timelines: {
+    $url: (url?: { hash?: string }) => ({
+      pathname: "/timelines" as const,
+      hash: url?.hash,
+      path: `/timelines${buildSuffix(url)}`,
+    }),
   },
-  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash, path: `/${buildSuffix(url)}` })
+  $url: (url?: { hash?: string }) => ({
+    pathname: "/" as const,
+    hash: url?.hash,
+    path: `/${buildSuffix(url)}`,
+  }),
 };
 
 export type PagesPath = typeof pagesPath;
 
 export const staticPath = {
   images: {
-    logo_png: '/images/logo.png'
-  }
+    logo_png: "/images/logo.png",
+  },
 } as const;
 
 export type StaticPath = typeof staticPath;
